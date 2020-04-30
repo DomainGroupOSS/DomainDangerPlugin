63888330b5db345f3f2d8fdf77219ce011bc4719
Dan Rodrigues <dan.rodrigues@domain.com.au>
2020-04-30 11:03:53 +1000

    Dependency updates for swift 5.2

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/danger/swift.git", from: "3.3.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
-        .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "0.3.0")
+        .package(url: "https://github.com/f-meloni/danger-swift-coverage", from: "1.1.0")
         // Dev dependencies
     ],
     targets: [
